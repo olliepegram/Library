887aafe6f546b86e5b4661dde2b9d43bbc13c329
Ollie Pegram <olliepegram@gmail.com>
2016-09-20 13:20:41 +1000

    implemented book model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+         #User can have many books
+         has_many :books
 end
